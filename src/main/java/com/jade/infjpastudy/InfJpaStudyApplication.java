6db931bb5102541908a9741ec89fb77d2fc4707c
sshinn0224 <sshinn0224@gmail.com>
2025-08-10 13:03:30 +0900

    feat : first commit

---
+++ src/main/java/com/jade/infjpastudy/InfJpaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jade.infjpastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfJpaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfJpaStudyApplication.class, args);
+    }
+
+}
